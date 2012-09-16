189f8c6385d6b4467efcf3752ab6a256970d348c
Roshan Piyush <piyushroshan@gmail.com>
2012-09-16 21:33:12 +0200

    tinymce error

--- tedxnitcalicut/settings.py
+++ tedxnitcalicut/settings.py
@@ -179,6 +179,8 @@ INSTALLED_APPS = (
     'oauth2',
 )
 
+FILEBROWSER_DEBUG=True
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
