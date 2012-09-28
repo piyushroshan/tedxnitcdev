bc8b64a1170b6005dd42d13d8447081022f0b86b
piyushroshan <piyushroshan@gmail.com>
2012-09-29 02:07:48 +0530

    Email added

--- nominations/manage.py
+++ nominations/manage.py
@@ -12,6 +12,13 @@ def nominated(request):
 	if request.method == 'POST':
 		form = NominationForm(request.POST)
 		if form.is_valid():
+			from django.core.mail import *
+			current_domain = Site.objects.get_current().domain
+			subject = "New nomination at %s" % current_domain
+			message_template = loader.get_template('nominations/nomination_email.txt')
+			message_context = Context({ 'form': form })
+			message = message_template.render(message_context)
+			EmailMessage(subject, message, settings.DEFAULT_FROM_EMAIL, [new_user.email])
 			form.save()
 			return HttpResponseRedirect('/')
 		else:
