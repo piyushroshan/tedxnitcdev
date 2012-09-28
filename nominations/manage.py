22ab9830d9bd7262e1bbe809ded90ab7053da685
piyushroshan <piyushroshan@gmail.com>
2012-09-29 02:19:10 +0530

    Email added

--- nominations/manage.py
+++ nominations/manage.py
@@ -2,7 +2,7 @@ from django.http import *
 from django import forms
 from django.conf import settings
 from django.template.loader import get_template
-from django.template import RequestContext
+from django.template import RequestContext, Context
 from django.forms import ModelForm
 from nominations.models import *
 from nominations.forms import *
